2c17c112c6b8fa900706ee3048bf3327c612f8b1
mskmrv <Mx123@yandex.ru>
2019-04-08 20:54:28 +1000

    Первый коммит

---
+++ core/src/ru/geekbrains/StarGame.java
@@ -0,0 +1,41 @@
+package ru.geekbrains;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	TextureRegion region;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+//		img = new Texture("badlogic.jpg");
+		img = new Texture("space.png");
+//		region = new TextureRegion(img, 100, 100, 150, 150);
+//		region.getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+//		batch.setColor(0.454f, 0.151f, 0.959f, 1);
+		batch.draw(img, 0, 0);
+//		batch.setColor(0.178f, 0.266f,0.366f, 0.5f);
+//		batch.draw(region, 300, 300);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
