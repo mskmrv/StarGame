441d509773c04f0639917c1abac2c93a4e3e633c
Maksim <mx123@yandex.ru>
2019-04-16 16:10:38 +1000

    Воспроизвел код урока ч2.

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -7,30 +7,46 @@ import com.badlogic.gdx.math.Vector2;
 import ru.geekbrains.base.BaseScreen;
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture bg;
+    private Texture textureLogo;
     private Background background;
+    private Logo logo;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("bg.png");
+        textureLogo = new Texture("badlogic.jpg");
         background = new Background(new TextureRegion(bg));
+        logo = new Logo(new TextureRegion(textureLogo));
     }
 
     @Override
     public void resize(Rect wordBounds) {
         super.resize(wordBounds);
         background.resize(wordBounds);
+        logo.resize(wordBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        update(delta);
+        draw();
+    }
+
+    private void update(float delta) {
+        logo.update(delta);
+    }
+
+    private void draw() {
         batch.begin();
         background.draw(batch);
+        logo.draw(batch);
         batch.end();
     }
 
@@ -38,10 +54,12 @@ public class MenuScreen extends BaseScreen {
     public void dispose() {
         super.dispose();
         bg.dispose();
+        textureLogo.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer) {
+        logo.touchDown(touch, pointer);
         return false;
     }
 }
---
+++ core/src/ru/geekbrains/sprite/Logo.java
@@ -0,0 +1,56 @@
+package ru.geekbrains.sprite;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.base.Sprite;
+import ru.geekbrains.math.Rect;
+
+public class Logo extends Sprite {
+
+    private Vector2 touch;
+    private Vector2 v;
+    private Vector2 buf;
+    private Vector2 bufV;
+
+    public Logo(TextureRegion region) {
+        super(region);
+        setHeightProportion(0.3f);
+        touch = new Vector2();
+        v = new Vector2();
+        buf = new Vector2();
+        bufV = new Vector2();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        setHeightProportion(0.3f);
+    }
+
+    @Override
+    public void update(float delta) { // delta - временной промежуток
+        super.update(delta);
+        buf.set(touch);
+        bufV.set(v);
+        bufV.scl(delta);
+        if (buf.sub(pos).len() <= bufV.len()) {
+            pos.set(touch);
+        } else {
+            pos.mulAdd(v, delta);
+        }
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        super.draw(batch);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        this.touch = touch;
+        v.set(touch.cpy().sub(pos)).setLength(0.05f);
+        return super.touchDown(touch, pointer);
+    }
+}
